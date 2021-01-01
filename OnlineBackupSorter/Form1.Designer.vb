96bcfc1678978c244a532a7eebffaaa4e312753d
soaresden <54243866+soaresden@users.noreply.github.com>
2021-01-01 17:00:52 +0100

    Ajoutez des fichiers projet.

---
+++ OnlineBackupSorter/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form remplace la méthode Dispose pour nettoyer la liste des composants.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requise par le Concepteur Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'REMARQUE : la procédure suivante est requise par le Concepteur Windows Form
+    'Elle peut être modifiée à l'aide du Concepteur Windows Form.  
+    'Ne la modifiez pas à l'aide de l'éditeur de code.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.VideoPlayer1 = New AxWMPLib.AxWindowsMediaPlayer()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.VideoPlayer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(7, 8)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(609, 20)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.Text = "D:\Documents\MEGAsync\Camera Uploads\DCIM Mi8\screenshots"
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(10, 36)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.ListBox1.Size = New System.Drawing.Size(251, 459)
+        Me.ListBox1.TabIndex = 2
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(267, 36)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(349, 511)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 4
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(624, 5)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Importer"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(624, 52)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox3.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(621, 36)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Nouveau Dossier :"
+        '
+        'ListBox3
+        '
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.Location = New System.Drawing.Point(730, 36)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(69, 511)
+        Me.ListBox3.TabIndex = 10
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(624, 78)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(100, 36)
+        Me.Button3.TabIndex = 11
+        Me.Button3.Text = "Creer le dossier"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(624, 120)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(100, 54)
+        Me.Button4.TabIndex = 12
+        Me.Button4.Text = "Supprimer le dossier select ci-droite"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(120, 501)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(141, 52)
+        Me.Button5.TabIndex = 13
+        Me.Button5.Text = "Deplacer les fichiers vers le dossier selectionné (à droite)"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'VideoPlayer1
+        '
+        Me.VideoPlayer1.Enabled = True
+        Me.VideoPlayer1.Location = New System.Drawing.Point(267, 36)
+        Me.VideoPlayer1.Name = "VideoPlayer1"
+        Me.VideoPlayer1.OcxState = CType(resources.GetObject("VideoPlayer1.OcxState"), System.Windows.Forms.AxHost.State)
+        Me.VideoPlayer1.Size = New System.Drawing.Size(348, 511)
+        Me.VideoPlayer1.TabIndex = 14
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(10, 501)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(104, 52)
+        Me.Button6.TabIndex = 15
+        Me.Button6.Text = "Supprimer les fichiers select."
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(10, 559)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(251, 20)
+        Me.TextBox2.TabIndex = 16
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(730, 559)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(69, 20)
+        Me.TextBox4.TabIndex = 17
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(806, 591)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.VideoPlayer1)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Margin = New System.Windows.Forms.Padding(2)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.VideoPlayer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents VideoPlayer1 As AxWMPLib.AxWindowsMediaPlayer
+    Friend WithEvents Button6 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+End Class
---
+++ OnlineBackupSorter/Form1.vb
@@ -0,0 +1,139 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call importFichiers1()
+        Call ImportListeDossier1()
+        MsgBox("Import OK")
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Dim dernierchoix = ListBox1.SelectedItems(ListBox1.SelectedItems.Count - 1).ToString()
+        TextBox2.Text = dernierchoix
+
+        If ListBox1.SelectedItems.Count > 1 Then
+            VideoPlayer1.Ctlcontrols.stop()
+            VideoPlayer1.URL = Nothing
+            If Path.GetExtension(TextBox2.Text) = ".mp4" Then Exit Sub
+        End If
+
+        If Path.GetExtension(TextBox2.Text) = ".jpg" Then
+            PictureBox1.Show()
+
+            Dim img As Image = Image.FromFile(TextBox1.Text & "\" & TextBox2.Text)
+            PictureBox1.Image = New Bitmap(img)
+            img.Dispose()
+
+            VideoPlayer1.Hide()
+            VideoPlayer1.Ctlcontrols.stop()
+            VideoPlayer1.URL = Nothing
+        ElseIf Path.GetExtension(ListBox1.SelectedItem.ToString) = ".mp4" Then
+            PictureBox1.Hide()
+
+            VideoPlayer1.Show()
+            VideoPlayer1.URL = TextBox1.Text & "\" & ListBox1.SelectedItem
+            VideoPlayer1.Ctlcontrols.play()
+        End If
+
+        'refresh list dossiers
+        Call ImportListeDossier1()
+
+    End Sub
+
+    Sub ImportListeDossier1()
+        ListBox3.Items.Clear()
+
+        For Each foundDirectory In Directory.GetDirectories(TextBox1.Text, ".", SearchOption.TopDirectoryOnly)
+            Dim dossier As String = System.IO.Path.GetFileName(foundDirectory)
+            ListBox3.Items.Add(dossier)
+        Next
+
+    End Sub
+    Sub ImportFichiers1()
+        'raz par precaution
+        ListBox1.Items.Clear()
+
+        'On boucle sur les fichiers et on ajoute a la liste
+        Dim di As New IO.DirectoryInfo(TextBox1.Text)
+        Dim aryFi As IO.FileInfo() = di.GetFiles("*", SearchOption.TopDirectoryOnly)
+        Dim fi As IO.FileInfo
+
+        For Each fi In aryFi
+            ListBox1.Items.Add(fi.Name)
+        Next
+    End Sub
+    Private Sub PictureBox1_DoubleClick(sender As Object, e As EventArgs) Handles PictureBox1.DoubleClick
+        Process.Start(TextBox1.Text & "\" & ListBox1.SelectedItem)
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        'On check si ca existe, au cas ou on le cree
+        If (Not System.IO.Directory.Exists(TextBox1.Text & "\" & TextBox3.Text)) Then
+            System.IO.Directory.CreateDirectory(TextBox1.Text & "\" & TextBox3.Text)
+        End If
+        ListBox3.Items.Add(TextBox3.Text)
+        TextBox3.Text = Nothing
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim path As String = TextBox1.Text & "\" & ListBox3.SelectedItem
+        System.IO.Directory.Delete(path, True)
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+
+        If ListBox1.SelectedItems.Count > 1 Then
+            VideoPlayer1.Ctlcontrols.stop()
+        End If
+
+        For Each itm As String In ListBox1.SelectedItems
+            Dim path As String = TextBox1.Text & "\" & itm.ToString
+            System.IO.File.Delete(path)
+        Next
+
+        PictureBox1.Image = Nothing
+        VideoPlayer1.URL = Nothing
+
+        Call importFichiers1()
+
+        If ListBox1.Items.Count = 0 Then
+            ListBox1.ClearSelected()
+        Else
+            ListBox1.SelectedIndex = 0
+            ListBox3.SelectedItem = TextBox4.Text
+        End If
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        VideoPlayer1.settings.volume = 100
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If ListBox3.SelectedItems.Count = 0 Then Exit Sub
+
+        VideoPlayer1.URL = Nothing
+
+        For Each itm As String In ListBox1.SelectedItems
+            Dim cheminoriginal As String = TextBox1.Text & "\" & itm.ToString
+            Dim cheminfinal As String = TextBox1.Text & "\" & ListBox3.SelectedItem & "\" & itm.ToString
+
+            On Error Resume Next
+            File.Move(cheminoriginal, cheminfinal)
+            On Error GoTo 0
+        Next
+
+        Call importFichiers1()
+
+        If ListBox1.Items.Count = 0 Then
+            ListBox1.ClearSelected()
+        Else
+            ListBox1.SelectedIndex = 0
+            ListBox3.SelectedItem = TextBox4.Text
+        End If
+    End Sub
+
+    Private Sub ListBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox3.SelectedIndexChanged
+        Dim dernierchoix = ListBox3.SelectedItems(ListBox3.SelectedItems.Count - 1).ToString()
+        TextBox4.Text = dernierchoix
+    End Sub
+End Class
